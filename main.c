1ba3cba22cfd7bf60bd3dcc50877ecda7d3891ba
Nick536363 <3383371nik@gmail.com>
2026-07-01 06:02:26 +0200

    Added checking for correct number of arguments + opening source file

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>  // for perror only
 
-int main(void){
+
+int main(int argc, char* argv[]){
+    if(argc == 1 || argc == 2){
+        puts("Too few arguments");
+        exit(EXIT_FAILURE);
+    }
+    
+    
+    int source_fd = open(argv[1], O_RDONLY);
+    if(source_fd == -1)
+        perror(argv[1]);
 
     return 0;
 }
